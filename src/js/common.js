c8fd859e24ed1525510bd37399785908d99c5995
Ex-Zy <serega1905@ukr.net>
2016-03-09 14:18:04 +0200

    add slider

--- src/js/common.js
+++ src/js/common.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-	$blocks = $(".advantage__unit")
-		$buttons = $(".advantage__top .btn")
+	$blocks = $(".advantage__unit");
+	$buttons = $(".advantage__top .btn");
 
-		$buttons.click(function(event) {
-			if(!$(this).hasClass('is-active')){
-				$buttons.removeClass('is-active');
-				$blocks.hide();
+	$buttons.click(function(event) {
+		if(!$(this).hasClass('is-active')){
+			$buttons.removeClass('is-active');
+			$blocks.hide();
 
-				$(this).addClass('is-active');
-				$('#'+$(this).data('target')).show();
-			}
-		});
+			$(this).addClass('is-active');
+			$('#'+$(this).data('target')).show();
+		}
+	});
+	$('.js-slider-top').slick({
+		slidesToShow: 1,
+		cssEase: 'linear',
+		fade: true,
+	});
+	$('.slider-nav__thumb').on('click', function() {
+		var id = $(this).index();
+		$('.slider-top').slick('slickGoTo', id);
+	});
 });
